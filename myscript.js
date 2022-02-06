7b18076d63f7e0998ef61aba55174094a98c97b9
avinash-finurja <avinash@finurja.com>
2022-02-07 00:06:18 +0530

    Modifying the layout and the copytext.

---
+++ myscript.js
@@ -0,0 +1,9 @@
+// Get the modal
+
+
+function displayMessage() {
+    let modal = document.getElementById("toast_message");
+    let buttonContainer = document.getElementById("container")
+    modal.style.display = "block"
+    buttonContainer.style.display = "none"
+}
